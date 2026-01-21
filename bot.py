8afc96a4b1eac8d18618f91052ad3d28df2cb188
lyrixmelodia <lyrical0lyrix@gmail.com>
2026-01-21 06:25:04 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix="!")  # You can change "!" to anything
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
